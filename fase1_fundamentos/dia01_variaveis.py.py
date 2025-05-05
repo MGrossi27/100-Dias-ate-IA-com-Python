17f8a64d4ab3371ec4c732b1fada6cbf296c56ed
Michael Grossi Farias <134861649+MGrossi27@users.noreply.github.com>
2025-05-05 12:18:54 -0300

    Add files via upload

---
+++ fase1_fundamentos/dia01_variaveis.py.py
@@ -0,0 +1,11 @@
+# Dia 1 - Variáveis e Tipos
+
+# Declarando variáveis
+nome = "Michael"
+idade = 24
+gosta_de_ia = True
+
+# Exibindo informações
+print("Nome:", nome)
+print("Idade:", idade)
+print("Gosta de IA:", gosta_de_ia)
