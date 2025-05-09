c7a04d2bb43871469182762e10f61d4b1d219824
Michael Grossi Farias <134861649+MGrossi27@users.noreply.github.com>
2025-05-09 12:53:18 -0300

    Dia 5 — Desafio do Formulário
    ✅ Pedir nome, idade e se já programou
    ✅ Exibir um resumo formatado

---
+++ fase1_fundamentos/dia05_desafio_formulario.py
@@ -0,0 +1,11 @@
+
+# Dia 5 - Desafio Formulário
+
+nome = input("Digite seu nome: ")
+idade = input("Digite sua idade: ")
+programou = input("Você já programou antes? (Sim/Não): ")
+
+print("\nResumo:")
+print("Nome:", nome)
+print("Idade:", idade)
+print("Já programou antes:", programou)
