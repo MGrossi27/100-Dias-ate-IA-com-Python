5bfda216096640b00ad73896cb341911d67d58bc
Michael Grossi Farias <134861649+MGrossi27@users.noreply.github.com>
2025-05-16 11:17:26 -0300

    Dia 12 — Argumentos e retorno
    ✅ Define função somar(a, b)
    ✅ Recebe entrada do usuário
    ✅ Usa return

---
+++ fase1_fundamentos/Dia12_argumentos_retorno.py
@@ -0,0 +1,11 @@
+
+# Dia 12 - Argumentos e Retorno
+
+def somar(a, b):
+    return a + b
+
+n1 = int(input("Digite o primeiro número: "))
+n2 = int(input("Digite o segundo número: "))
+resultado = somar(n1, n2)
+
+print("Resultado da soma:", resultado)
