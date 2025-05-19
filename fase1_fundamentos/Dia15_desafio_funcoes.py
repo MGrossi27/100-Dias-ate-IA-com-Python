e16765854e361de8fb05601b7651fedf36f90157
Michael Grossi Farias <134861649+MGrossi27@users.noreply.github.com>
2025-05-19 14:10:44 -0300

    Dia 15 — Desafio com funções
    ✅ Recebe nome, idade e hobby
    ✅ Usa função para exibir frase personalizada

---
+++ fase1_fundamentos/Dia15_desafio_funcoes.py
@@ -0,0 +1,12 @@
+
+# Dia 15 - Desafio com Funções
+
+def exibir_resumo(nome, idade, hobby):
+    print(f"Olá, {nome}!")
+    print(f"Você tem {idade} anos e gosta de {hobby}.")
+
+nome = input("Digite seu nome: ")
+idade = input("Digite sua idade: ")
+hobby = input("Digite seu hobby favorito: ")
+
+exibir_resumo(nome, idade, hobby)
