a1afd500e11626d5484fcb93e8af87b49b5e85da
Michael Grossi Farias <134861649+MGrossi27@users.noreply.github.com>
2025-05-23 11:50:43 -0300

    Dia 19 — Manipulação de strings
    ✅ Inverte frase com slicing
    ✅ Usa .upper() e .lower()

---
+++ fase1_fundamentos/Dia19_strings.py
@@ -0,0 +1,9 @@
+
+# Dia 19 - Manipulação de Strings
+
+frase = input("Digite uma frase: ")
+invertida = frase[::-1]
+
+print("Frase invertida:", invertida)
+print("Maiúscula:", frase.upper())
+print("Minúscula:", frase.lower())
