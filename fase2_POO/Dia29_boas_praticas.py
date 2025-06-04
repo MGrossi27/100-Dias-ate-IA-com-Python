2875088f9d9b636c30d66206054e76290b6ac18e
Michael Grossi Farias <134861649+MGrossi27@users.noreply.github.com>
2025-06-04 18:24:06 -0300

    Dia 29 — Boas Práticas
    ✅ Função tipada com float
    ✅ Comentários/docstring explicativos

---
+++ fase2_POO/Dia29_boas_praticas.py
@@ -0,0 +1,8 @@
+
+# Dia 29 - Boas Práticas
+
+def calcular_area(base: float, altura: float) -> float:
+    """Calcula a área de um retângulo."""
+    return base * altura
+
+print("Área:", calcular_area(5, 10))
